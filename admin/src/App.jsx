a3fe486b2cdf52e955ae158f128492955a942a64
Moldovan Ioana Ilinca <miie2774@scs.ubbcluj.ro>
2022-06-08 17:57:06 +0300

    Refactoring 3.2 deleted newUser

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -5,7 +5,6 @@ import Home from "./pages/home/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import UserList from "./pages/userList/UserList";
 import User from "./pages/user/User";
-import NewUser from "./pages/newUser/NewUser";
 import ProductList from "./pages/productList/ProductList";
 import Product from "./pages/product/Product";
 import NewProduct from "./pages/newProduct/NewProduct";
@@ -62,9 +61,6 @@ function App() {
                 </Route>
                 <Route path="/user/:userId">
                   <User />
-                </Route>
-                <Route path="/newUser">
-                  <NewUser />
                 </Route>
                 <Route path="/products">
                   <ProductList />
